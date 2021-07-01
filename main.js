0ff74ff5c43be6e3044b973f9885f6c8727789eb
edanielgabor <84292330+edanielgabor@users.noreply.github.com>
2021-07-01 14:39:41 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const restaurant = ["Westy", "PadThai", "Peti's Döner", "Kebap Ház", "Fei-Fei Kínai", "Chili's Burger", "Arizona", "Bolero"];
+const restaurant = ["WESTY Zöld FUTÁR", "WESTY Ebéd menü", "WESTY Burger", "Chili's Burger","Barney's American Diner", "HÚSEVŐ Bistro & Steak bar", "Peti's Döner Kebap & Sandwich", "Kebap Ház", "Fei Fei Kínai Ételbár", "Padthai Wokbar" "McDonald's®", "BURGER KING®", "KFC", "Dolce Vita", "Fatányéros Étterem"];
 const getRandomNumber = (max) => Math.floor(Math.random() * max);
 const getRandomName = () => 
   `${restaurant[getRandomNumber(restaurant.length)]}`;
